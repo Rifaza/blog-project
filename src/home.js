9e2aa7596e2a78de65e34701fec208c37c711cb4
Anas Fathima Rifaza <afrifaza17@gmail.com>
2024-12-16 22:18:49 +0530

    added delete blog

--- src/home.js
+++ src/home.js
@@ -18,17 +18,22 @@ const home = () => {
         console.log('Rifaza ' + name ,  e.target);
     }
 
+
   // eslint-disable-next-line react-hooks/rules-of-hooks
     const [blogs, setBlogs] = useState([
         {title : 'Finding the destiny', body :'This is new journey...', author : 'Rifaza', id :1},
         {title : 'Success Life', body :'One a upon time in summe...', author : 'Anas', id :2},
         {title : 'Fuilure to success', body :'There are many factors...', author : 'Rifaza', id :3},
     ])
+    const handleDelete = (id) =>{
+        const newBlogs= blogs.filter((blog)=> blog.id !== id)
+        setBlogs(newBlogs);
 
+    }
     return (  
         <div  className="home">
-            <Bloglist blogs = {blogs} title="All Blogs"/>
-            <Bloglist blogs = {blogs.filter((blog)=> blog.author=== 'Rifaza')} title="Rifaza's Blogs"/>
+            <Bloglist blogs = {blogs} title="All Blogs" handleDelete={handleDelete}/>
+            <Bloglist blogs = {blogs.filter((blog)=> blog.author=== 'Rifaza')}  title="Rifaza's Blogs"/>
         </div>
     );
 }
