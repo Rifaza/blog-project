dbfedf2ea980a094d1642afe1251f23bbd364825
Anas Fathima Rifaza <afrifaza17@gmail.com>
2024-12-20 19:35:48 +0530

    custom hook added

--- src/home.js
+++ src/home.js
@@ -1,55 +1,22 @@
 import {  useEffect, useState } from "react";
-// eslint-disable-next-line react-hooks/rules-of-hooks
 import Bloglist from "./Bloglist.js";
+import useFetch from "./useFetch.js";
 
 const Home = () => {
 
-    const [name, setName] = useState('Rifaza');
-    const [age, setAge] = useState(25);
-    const [isPending, setIsPending] = useState(true);
-    const handleClick = (e) => {
-        setName('Fathima')
-        setAge(30);
-    }
+    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
-
-    const handleClick1 = (name, e) => {
-        console.log('Rifaza ' + name ,  e.target);
-    }
-
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [blogs, setBlogs] = useState(null)
     const handleDelete = (id) =>{
         const newBlogs= blogs.filter((blog)=> blog.id !== id)
-        setBlogs(newBlogs);
-
     }
-
-    useEffect(()=>{
-  setTimeout(()=>{
-    fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(
-            data=>{
-                console.log(data);
-                setBlogs(data);
-                
-                setIsPending(false);
-            }
-        )
-  }, 1000)
-    
-    }, []);
+       
     return (  
         <div  className="home">
+            { error && <div>{ error}</div>}
             {isPending && <div>Loading ...</div>}
             { blogs && <Bloglist blogs = {blogs} title="All Blogs" handleDelete={handleDelete}/>}
             {/* <Bloglist blogs = {blogs.filter((blog)=> blog.author=== 'Rifaza')}  title="Rifaza's Blogs"/> */}
-            {/* <button onClick={handleClick}> Change Name</button>
-            <p>{name}</p> */}
+          
         </div>
     );
 }
