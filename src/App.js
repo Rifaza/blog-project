03c7d94d82047e7c44b27d4906f65f3b5f5adaa5
Anas Fathima Rifaza <afrifaza17@gmail.com>
2024-12-21 09:05:18 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
 import Navbar from './navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+<Router>
+<div className="App">
       <Navbar/>
       <div className='content'>
-        <Home/>
+        <Switch>
+          <Route exact path="/">
+           <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+        </Switch>
+     
       </div>
     </div>
+</Router>
   );
 }
 
