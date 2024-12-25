afe7889d291810612cdaf786ba264e278450a14d
Anas Fathima Rifaza <afrifaza17@gmail.com>
2024-12-25 10:08:26 +0530

    Creating a new blog

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+
+const [title, setTitle]  =  useState('')
+const [body, setBody]  =  useState('')
+const [author, setAuthor]  =  useState('Rifaza')
+
     return ( 
-        <div className="craete">
-            <h2>Add a new Blog</h2>
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input type="text" required value={title}
+                onChange={(e) => setTitle(e.target.value)}></input>
+
+                <label>Blog body:</label>
+                <textarea  required value={body}
+                 onChange={(e) => setBody(e.target.value)}/>
+
+                <label>Blog author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Rifaza">Rifaza</option>
+                    <option value="Anas">Anas</option>
+                </select>
+                <br/>
+                <button> Add Blog </button>
+
+                <br/>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
      );
 }
